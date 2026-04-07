b84b0bb795f7fa32fd4f114632ccc67c954a4dd2
Alamino Code Solutions <alamino.code.solutions@gmail.com>
2026-04-07 00:36:26 +0000

    atualizar  no vercel

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home(){
     return (
         <>
-            <h1>Lucy, esta é uma página que estou fazendo com o curso.dev</h1>
+            <h1><strong>Lucy</strong>, esta é uma página que estou fazendo com o curso.dev</h1>
             <p>Nela vou usar esse espaço pra dizer que te amo muito, que tenho muito orgulho de você e que sou completamente apaixonado por você.</p>
             <p>E se você gostou, dê uma risadinha 😊​</p>
         </>
